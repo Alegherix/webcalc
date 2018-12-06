81ecfe7d53978ba71a8b708dab21f65ef8586bde
Martin <psao69@gmail.com>
2018-12-06 15:47:24 +0100

    Updated PostFix with testing Closing and Opening Paranthesis

--- src/calc/Postfix.java
+++ src/calc/Postfix.java
@@ -6,47 +6,77 @@ import java.util.Stack;
 
 public class Postfix {
     private List<String> expr;
-    private Stack<Character> stack;
+    private Stack<String> stack;
 
 
     public Postfix(List<String> exprToEvaluate) {
-        stack = new Stack();
+        stack = new Stack<>();
         expr = exprToEvaluate;
     }
 
-    public int getPrecedence(char op){
-     if('+'==op || '-'==op){
-         return 1;
-     }
-     else if('*'==op || '/'==op){
-         return 2;
-     }
-     else if('^'==op){
-         return 3;
-     }
-     else{
-         return 0;
-     }
+    public int getPrecedence(String op){
+        switch (op){
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+            case "/":
+                return 2;
+            case "*":
+                return 2;
+            case "^":
+                return 3;
+            default:
+                return 0;
+         }
     }
 
-    public boolean isOperator(char op){
+    public boolean isOperator(String op){
         switch (op){
-            case '+':
+            case "+":
                 return true;
-            case '-':
+            case "-":
                 return true;
-            case '/':
+            case "/":
                 return true;
-            case '*':
+            case "*":
                 return true;
-            case '^':
+            case "^":
                 return true;
                 default:
                     return false;
         }
     }
 
-    public void putOnStack(char op){
+    public void handleExpression(List<String> inputList){
+        /**
+         * Om Det är en operator, Så utför ett antal checks och basera därefter
+         * 1) Om det är en bracket som stänger, poppa då ut allt innanför bracketsen
+         * 2) Om det är en Operator med lägre Precedence, poppa då ut den med högre precedence, gör sedan om
+         * Checken, och poppa ut samtliga tills dess att det närliggande operatorn:
+         *      - Inte finns
+         *      - Är en Bracket
+         *      - Har lägre precedence
+         *
+         */
+        for (int i = 0; i < inputList.size(); i++) {
+            String toBeOperatedOn = inputList.get(i);
+
+            if(isOperator(toBeOperatedOn)){
+
+            }
+        }
+    }
+
+    public boolean isClosingOperator(String operator){
+        return "(".equals(operator);
+    }
+
+    public boolean isOpeningOperator(String operator){
+        return ")".equals(operator);
+    }
+
+    public void putOnStack(String op){
         stack.push(op);
     }
 
