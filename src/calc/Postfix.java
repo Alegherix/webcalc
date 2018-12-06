02dd88a43dbc85dbaaaa0c42a843e6baf4397a5b
Martin <psao69@gmail.com>
2018-12-06 16:06:18 +0100

    Added extra Todos

--- src/calc/Postfix.java
+++ src/calc/Postfix.java
@@ -1,8 +1,6 @@
 package calc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Postfix {
     private List<String> expr;
@@ -48,25 +46,41 @@ public class Postfix {
         }
     }
 
-    public void handleExpression(List<String> inputList){
+    public void handleExpression(){
         /**
          * Om Det är en operator, Så utför ett antal checks och basera därefter
-         * 1) Om det är en bracket som stänger, poppa då ut allt innanför bracketsen
-         * 2) Om det är en Operator med lägre Precedence, poppa då ut den med högre precedence, gör sedan om
+         * 1) Om det är en brackets som öppnar, Pusha de till stacken.
+         * 2) Om det är en bracket som stänger, poppa då ut allt innanför bracketsen
+         * 3) Om det är en Operator med lägre Precedence, poppa då ut den med högre precedence, gör sedan om
          * Checken, och poppa ut samtliga tills dess att det närliggande operatorn:
          *      - Inte finns
          *      - Är en Bracket
          *      - Har lägre precedence
          *
          */
-        for (int i = 0; i < inputList.size(); i++) {
-            String toBeOperatedOn = inputList.get(i);
+        for (int i = 0; i < this.expr.size(); i++) {
+            String toBeOperatedOn = this.expr.get(i);
+            if(isOpeningOperator(toBeOperatedOn)){
+                putOnStack(toBeOperatedOn);
+            }
+            else if(isClosingOperator(toBeOperatedOn)){
+                // Plocka ut De operatorer som finns mellan (, och )
+                // Placera dessa I listan
+                // Poppa sedan ( && ) ur stacken.
+            }
+            else if(isOperator(toBeOperatedOn)){
+                // Här kommer majoriteten av all logik att finnas.
 
-            if(isOperator(toBeOperatedOn)){
-
+            }
+            else if(!isOperator(toBeOperatedOn) && !isClosingOperator(toBeOperatedOn) && isOpeningOperator(toBeOperatedOn)){
+                // Måste vara ett nummer, placera i listan
+                putInToList(toBeOperatedOn);
             }
         }
     }
+
+    //Todo Beräkna vilka operatorer som finns mellan ( )
+
 
     public boolean isClosingOperator(String operator){
         return "(".equals(operator);
@@ -84,8 +98,19 @@ public class Postfix {
         expr.add(character);
     }
 
+    public List<String> getExpr() {
+        return expr;
+    }
+
+    public Stack<String> getStack() {
+        return stack;
+    }
 
     public static void main(String[] args) {
+        List<String> testList = Arrays.asList("1","2","3");
+        Postfix testObj = new Postfix(testList);
+        testObj.handleExpression();
+        testObj.getExpr().stream().forEach(System.out::print);
     }
 }
 
